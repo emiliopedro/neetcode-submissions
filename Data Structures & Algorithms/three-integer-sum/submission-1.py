74709721ed61e4f03cce3b75fcd74ac190c444d4
emiliopedro <p306535@dac.unicamp.br>
2026-07-25 16:51:48 -0300

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,31 @@
+def binarysearch(arr, inf, target):
+    sup = len(arr)-1
+
+    while inf <= sup:
+        mid = (inf + sup) // 2
+
+        if arr[mid] == target:
+            return mid
+        
+        if arr[mid] < target:
+            inf = mid + 1
+        elif arr[mid] > target:
+            sup = mid - 1
+
+    return False
+    
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        triplets = []
+        for i in range(len(nums)-2):
+            for j in range(i+1, len(nums)):
+                k = binarysearch(nums, j+1, -nums[i] - nums[j])
+                if k and [nums[i], nums[j], nums[k]] not in triplets:
+                    triplets.append([nums[i], nums[j], nums[k]])
+        
+        return triplets
+                
+                
